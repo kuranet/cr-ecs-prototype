ae7b4d8c2508fdcbbebee9f3c99cd7a7fdf6a7a1
Ann Kurenna <anna.kurennaya38@gmail.com>
2025-11-24 22:38:05 +0100

    Fixed: Physics - Check collision events and destroy ability objects on hit.

--- Assets/Scripts/BattleContext/Abilities/Components/Systems/AbilityMovementSystem.cs
+++ Assets/Scripts/BattleContext/Abilities/Components/Systems/AbilityMovementSystem.cs
@@ -1,6 +1,9 @@
 using Unity.Entities;
+using Unity.Physics.Systems;
 using Unity.Transforms;
 
+[UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
+[UpdateBefore(typeof(PhysicsSimulationGroup))]
 public partial struct AbilityMovementSystem : ISystem
 {
     public void OnUpdate(ref SystemState state)
@@ -8,7 +11,44 @@ public partial struct AbilityMovementSystem : ISystem
         foreach (var (transform, abilityMovement) in
                  SystemAPI.Query<RefRW<LocalTransform>, RefRO<AbilityMovement>>())
         {
+            var posBefore = transform.ValueRW.Position;
             transform.ValueRW.Position = transform.ValueRO.Position + abilityMovement.ValueRO.directionToTarget * abilityMovement.ValueRO.movingSpeed * SystemAPI.Time.DeltaTime;
+            //var entity = Raycast(posBefore, transform.ValueRW.Position);
+            //if (entity != Entity.Null)
+            //{
+            //    UnityEngine.Debug.LogError($"collide with something");
+            //}
         }
     }
+
+    //public Entity Raycast(float3 RayFrom, float3 RayTo)
+    //{
+    //    // Set up Entity Query to get PhysicsWorldSingleton
+    //    // If doing this in SystemBase or ISystem, call GetSingleton<PhysicsWorldSingleton>()/SystemAPI.GetSingleton<PhysicsWorldSingleton>() directly.
+    //    EntityQueryBuilder builder = new EntityQueryBuilder(Allocator.Temp).WithAll<PhysicsWorldSingleton>();
+
+    //    EntityQuery singletonQuery = World.DefaultGameObjectInjectionWorld.EntityManager.CreateEntityQuery(builder);
+    //    var collisionWorld = singletonQuery.GetSingleton<PhysicsWorldSingleton>().CollisionWorld;
+    //    singletonQuery.Dispose();
+
+    //    RaycastInput input = new RaycastInput()
+    //    {
+    //        Start = RayFrom,
+    //        End = RayTo,
+    //        Filter = new CollisionFilter()
+    //        {
+    //            BelongsTo = ~0u,
+    //            CollidesWith = ~0u, // all 1s, so all layers, collide with everything
+    //            GroupIndex = 0
+    //        }
+    //    };
+
+    //    RaycastHit hit = new RaycastHit();
+    //    bool haveHit = collisionWorld.CastRay(input, out hit);
+    //    if (haveHit)
+    //    {
+    //        return hit.Entity;
+    //    }
+    //    return Entity.Null;
+    //}
 }
--- Assets/Scripts/BattleContext/Abilities/Components/Systems/HitOnCollisionSystem.cs
+++ Assets/Scripts/BattleContext/Abilities/Components/Systems/HitOnCollisionSystem.cs
@@ -1,56 +1,69 @@
 using Unity.Collections;
 using Unity.Entities;
-using Unity.Mathematics;
-using Unity.Transforms;
+using Unity.Physics;
+using Unity.Physics.Systems;
 
+[UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
+[UpdateBefore(typeof(PhysicsSimulationGroup))]
 public partial struct HitOnCollisionSystem : ISystem
 {
     public void OnUpdate(ref SystemState state)
     {
+        var collisionEvents = SystemAPI.GetSingleton<SimulationSingleton>().AsSimulation().CollisionEvents;
+
         var ecb = new EntityCommandBuffer(Allocator.Temp);
 
-        foreach (var (transform, hitOnCollision, reference, entity) in
-                 SystemAPI.Query<RefRO<LocalTransform>, RefRO<HitOnCollision>, RefRO<CreationReference>>()
-                 .WithEntityAccess())
+        foreach (var collision in collisionEvents)
         {
-            foreach (var (otherTransform, otherEntity) in
-                     SystemAPI.Query<RefRO<LocalTransform>>()
-                     .WithAll<Health>()
-                     .WithEntityAccess())
+            if (GetHitObjectAndHitter(collision, state.EntityManager, out Entity attackEntity, out Entity targetEntity))
             {
-                // todo: if target self?
-                if (otherEntity == reference.ValueRO.Creator)
+                var stats = state.EntityManager.GetBuffer<StatsConfig>(attackEntity);
+                var damage = 0f;
+                foreach (var stat in stats)
                 {
-                    continue;
+                    if (stat.type == StatType.Damage)
+                    {
+                        damage += stat.addedValue;
+                    }
                 }
 
-                var distanceToOther = math.distance(transform.ValueRO.Position, otherTransform.ValueRO.Position);
-                if (distanceToOther < hitOnCollision.ValueRO.radius)
+                ecb.AddComponent(targetEntity, new AddDamage() { value = damage });
+                ecb.RemoveComponent<HitOnCollision>(attackEntity);
+
+                if (state.EntityManager.HasComponent<DestroyOnHit>(attackEntity))
                 {
-                    var stats = state.EntityManager.GetBuffer<StatsConfig>(entity);
-                    var damage = 0f;
-                    foreach(var stat in stats)
-                    {
-                        if(stat.type == StatType.Damage)
-                        {
-                            damage += stat.addedValue;
-                        }
-                    }
-                    
-                    ecb.AddComponent(otherEntity, new AddDamage() { value = damage });
-                    ecb.RemoveComponent<HitOnCollision>(entity);
-
-                    if (state.EntityManager.HasComponent<DestroyOnHit>(entity))
-                    {
-                        ecb.AddComponent(entity, new DestroyAfterDuration() { duration = 0.001f });
-                    }
-
-                    break;
+                    ecb.AddComponent(attackEntity, new DestroyAfterDuration() { duration = 0.001f });
                 }
             }
         }
 
         ecb.Playback(state.EntityManager);
         ecb.Dispose();
+    }
+
+    private bool GetHitObjectAndHitter(CollisionEvent e, EntityManager entityManager, out Entity attackEntity, out Entity targetEntity)
+    {
+        attackEntity = Entity.Null;
+        targetEntity = Entity.Null;
+
+        if (entityManager.HasComponent<HitOnCollision>(e.EntityA))
+        {
+            attackEntity = e.EntityA;
+        }
+        else if (entityManager.HasComponent<HitOnCollision>(e.EntityB))
+        {
+            attackEntity = e.EntityB;
+        }
+
+        if (entityManager.HasComponent<Health>(e.EntityA))
+        {
+            targetEntity = e.EntityA;
+        }
+        else if (entityManager.HasComponent<Health>(e.EntityB))
+        {
+            targetEntity = e.EntityB;
+        }
+
+        return attackEntity != Entity.Null && targetEntity != Entity.Null;
     }
 }
